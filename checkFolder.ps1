5c3c092420abd7b886fc14a6ac5c19755571f150
marvint24 <marvin.terme@gmail.com>
2022-10-22 01:25:50 +0200

    Initial commit

---
+++ checkFolder.ps1
@@ -0,0 +1,32 @@
+$orcPath=$env:ocrPATH
+
+Set-Location $orcPath
+
+while ($true) {
+    if(-not(Test-Path "Original")){
+        New-Item "Original" -ItemType Directory
+        chmod -R 777 /media/data
+    }
+
+    function getFiles {
+        return Get-ChildItem -Filter "*DoOcr*" -File
+    }
+    if(getFiles){
+        $file=(getFiles)[0]
+        $fileName=$file.Name
+        $newName=$fileName.Replace("DoOcr","RunningOcr")
+        Rename-Item $file.FullName $newName
+        $languages=$newName.Substring(10,$newName.IndexOf("-")-10)
+        switch ($languages) {
+            "De" {$ocrLanguages="deu"}
+            "En" {$ocrLanguages="eng"}
+            "DeEn" {$ocrLanguages="deu+eng"}
+        }
+        ocrmypdf -l $ocrLanguages $newName $newName.Replace("RunningOcr","")
+        Move-Item $newName "./Original/$fileName"
+    }
+    if(-not (getFiles)){
+        Start-Sleep -Seconds 30
+    }
+}
+
